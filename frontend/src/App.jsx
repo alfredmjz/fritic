665b6eef5887955d6be0f33242b9d0f6375b0294
alfredmjz <alfredmjz@gmail.com>
2022-09-09 18:42:48 -0400

    Add context api

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { RestaurantContextProvider } from "./context/RestaurantContext";
 import Home from "./router/Home";
 import Restaurant from "./router/Restaurant";
 import Update from "./router/Update";
 
 const App = () => {
 	return (
-		<div className='container'>
-			<BrowserRouter>
-				<Routes>
-					<Route exact path='/' element={<Home />} />
-					<Route exact path='/restaurant/:id/update' element={<Update />} />
-					<Route exact path='/restaurant/:id' element={<Restaurant />} />
-				</Routes>
-			</BrowserRouter>
-		</div>
+		<RestaurantContextProvider>
+			<div className='container'>
+				<BrowserRouter>
+					<Routes>
+						<Route exact path='/' element={<Home />} />
+						<Route exact path='/restaurant/:id/update' element={<Update />} />
+						<Route exact path='/restaurant/:id' element={<Restaurant />} />
+					</Routes>
+				</BrowserRouter>
+			</div>
+		</RestaurantContextProvider>
 	);
 };
 

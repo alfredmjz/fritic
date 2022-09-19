664aae98db932bb10cbd062537a903dd7e515d53
alfredmjz <alfredmjz@gmail.com>
2022-09-19 13:22:44 -0400

    fix minor error checking

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -7,23 +7,24 @@ loginRouter.post("/", async (req, res) => {
 	const { email, password } = req.body;
 
 	const user = await db.query("SELECT uuid, name, email, passwordhash FROM friticuser WHERE email = $1", [email]);
-	const passwordCorrect = user === null ? false : await bcrypt.compare(password, user.rows[0].passwordhash);
+	const passwordCorrect =
+		user.rows[0] === undefined ? false : await bcrypt.compare(password, user.rows[0].passwordhash);
 
-	if (!(user && passwordCorrect)) {
+	if (!(user.rows[0] && passwordCorrect)) {
 		return res.status(401).json({
-			error: "invalid username or password",
+			error: "invalid email or password",
 		});
 	}
 
 	const userForToken = {
-		email: user.email,
-		uuid: user.uuid,
+		email: user.rows[0].email,
+		uuid: user.rows[0].uuid,
 	};
 
 	// token expires in 3600 seconds; 1 hour
 	const duration = 3600;
 	const token = jwt.sign(userForToken, process.env.SECRET, { expiresIn: duration });
-	res.status(200).send({ token, email: user.email, name: user.name });
+	res.status(200).send({ token, email: user.rows[0].email, name: user.rows[0].name });
 });
 
 module.exports = loginRouter;

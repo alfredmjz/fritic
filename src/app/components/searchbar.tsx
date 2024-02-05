289e04f2236aaa719a72432b03d071a4e545fc05
Han Le <hanle.cs23@gmail.com>
2024-02-05 01:29:10 -0500

    Finished search bar UI

--- src/app/components/searchbar.tsx
+++ src/app/components/searchbar.tsx
@@ -1,20 +1,29 @@
 import React from "react";
 import VerticalLine from "@/public/svg/verticalLine.svg";
 import RightArrow from "@/public/svg/rightArrow.svg";
+import LocateMe from "@/public/svg/locateMe.svg";
 import Image from "next/image";
 
 interface SearchBarProps {}
 const NavBar = (props: SearchBarProps) => {
   return (
-    <div className="flex w-fit h-fit justify-center items-center">
-      <div className="flex bg-beige-color py-2.5 px-3 rounded-full text-sm justify-center items-center">
-        <input placeholder="Location" className="bg-beige-color"></input>
-        <Image src={VerticalLine} alt="Vertical Line" />
-        <input placeholder="Craving" className="bg-beige-color"></input>
-        <button className="bg-main-theme rounded-full w-fit h-fit px-2.5 py-3.5 flex justify-center items-center flex-shrink-0">
-          <Image src={RightArrow} alt="right arrow" height={10} width={7} />
-        </button>
-      </div>
+    <div className="flex bg-beige-color rounded-full text-sm min-w-fit h-fit justify-around items-center py-2.5 px-3 shrink-0">
+      <input
+        placeholder="Location"
+        className="bg-beige-color focus:outline-none w-1/6 shrink-0"
+      />
+      <button>
+        <Image src={LocateMe} alt="Locate Me" />
+      </button>
+
+      <Image src={VerticalLine} alt="Vertical Line" />
+      <input
+        placeholder="Craving"
+        className="bg-beige-color focus:outline-none shrink-0"
+      />
+      <button className="bg-main-theme rounded-full w-fit h-fit px-2.5 py-3.5 flex justify-center items-center flex-shrink-0">
+        <Image src={RightArrow} alt="right arrow" height={10} width={7} />
+      </button>
     </div>
   );
 };
